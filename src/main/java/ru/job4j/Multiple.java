276f7f98921dfe15d6b30cd4cb68c385b8f5fee1
jbioshock1912 <loh7457490>
2020-04-28 22:38:52 +0300

    Info update Multiple

--- src/main/java/ru/job4j/Multiple.java
+++ src/main/java/ru/job4j/Multiple.java
@@ -3,6 +3,10 @@ package ru.job4j;
 public class Multiple {
 
     public static void main(String[] args){
+        int age = 18;
+        float length =  78.0332;
+        boolean baby = true;
+
         System.out.println("1 * 2 = 2");
         System.out.println("1 * 3 = 3");
         System.out.println("1 * 9 = 9");
