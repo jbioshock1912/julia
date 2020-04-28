c5dfab0f3d2d8a8d9735f5ed41819b97e7f546df
jbioshock1912 <loh7457490>
2020-04-28 22:43:10 +0300

    Info update Multiple

--- src/main/java/ru/job4j/Multiple.java
+++ src/main/java/ru/job4j/Multiple.java
@@ -10,5 +10,24 @@ public class Multiple {
         System.out.println("1 * 2 = 2");
         System.out.println("1 * 3 = 3");
         System.out.println("1 * 9 = 9");
+
+        /*
+  -возраст int
+
+- пол boolean
+
+- рост double
+
+- количество кирпичей для постройки дома int
+
+- количество бактерий long
+
+- расстояние до космических объектов long
+
+- разрешение на работу boolean
+
+- размер файла. int
+
+*/
     }
 }
