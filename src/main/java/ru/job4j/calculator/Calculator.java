e2f6d44768b46ab5a7cb7cc1d57e0fe8c1e0522a
jbioshock1912 <loh7457490>
2020-04-29 22:37:02 +0300

    Calculator
    1. В методе main создайте дополнительные переменные, кроме тех, что уже созданы.
     - переменную типа int c именем six и значением 6.
     - переменную типа int c именем four и значением 4.
     - переменную типа int с именем five и значением 5.
    
    2. Создайте еще три переменные для хранения результата арифметических операций.
      - переменную типа int c именем sixDivTwo и присвойте ей значение арифметической операции деления переменной six на two.
      - переменную типа int c именем fiveMinusTwo и присвойте ей значение арифметической операции вычитания переменной five c two.
      - переменную типа int c именем fourTimeTwo и присвойте ей значение арифметической операции умножения переменной four на two.
    
    3. Выведите на консоль переменные sixDivTwo, fiveMinusTwo, fourTimeTwo.
    
    Задание
    
    1. В комментарии к этой задаче напишите код.
    
    В коде нужно объявить переменную size типа int и присвоить ей значение 10.
    
    Далее нужно назначить этой же переменной значение 100.
    
    Потом нужно отнять от переменной size число 5, и результат записать опять в переменную size.
    
    В вашем коде будет создана только одна переменная size.

--- src/main/java/ru/job4j/calculator/Calculator.java
+++ src/main/java/ru/job4j/calculator/Calculator.java
@@ -15,5 +15,11 @@ public class Calculator {
         System.out.println(sixDivTwo);
         System.out.println(fiveMinusTwo);
         System.out.println(fourTimeTwo);
+
+        int size = 10;
+        size = 100;
+        size = size - 5;
+        System.out.println(size);
+
     }
 }
