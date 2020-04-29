b6e7a625f9090682f5b867948f6b99ed806508f1
jbioshock1912 <loh7457490>
2020-04-29 12:52:36 +0300

    familiarity with variables

--- src/main/java/ru/job4j/Multiple.java
+++ src/main/java/ru/job4j/Multiple.java
@@ -4,7 +4,7 @@ public class Multiple {
 
     public static void main(String[] args){
         int age = 18;
-        float length =  78.0332;
+        float length =  78.0332F;
         boolean isBaby = true;
 
         System.out.println("1 * 2 = 2");
@@ -29,5 +29,42 @@ public class Multiple {
 - размер файла. int
 
 */
+        // задание Задание.
+        //
+        //1. Ниже приведен список переменных.
+        //
+        //- возраст
+        //
+        //- пол
+        //
+        //- рост
+        //
+        //- количество кирпичей для постройки дома
+        //
+        //- количество бактерий
+        //
+        //- расстояние до космических объектов
+        //
+        //- разрешение на работу
+        //
+        //- размер файла.
+        //
+        //Ваша задача создать переменную нужного типа, выбрать подходящее имя для переменной и присвоить произвольное значение.
+        //
+        //Решение написать прямо в комментарии.
+
+
+
+        int myAge = 29;
+        boolean isSexWomen = true;
+        int height = 161;
+        long numberOfBricks =  92233720368547758L;
+        long numberOfBacteria = 112254655115466261L;
+        long distance = 3232454654L;
+        boolean isWorkPermit = false;
+        int fileSize = 1245;
+
+
+
     }
 }
