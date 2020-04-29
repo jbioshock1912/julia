0c55200ef5d19d3690db8bee52aa2d2b2495a446
jbioshock1912 <loh7457490>
2020-04-29 22:54:07 +0300

    1. В комментарии к задаче напишите код, который вызывает метод plus с аргументами 100 и 500, 4 и 2, 3 и 5.

--- src/main/java/ru/job4j/calculator/Calculator.java
+++ src/main/java/ru/job4j/calculator/Calculator.java
@@ -1,25 +1,16 @@
 package ru.job4j.calculator;
 
 public class Calculator {
+    public static void plus(int first, int second) {
+        int result = first + second;
+        System.out.println(result);
+    }
+
     public static void main(String[] args) {
-        int one = 1;
-        int two = 2;
-        int six = 6;
-        int four = 4;
-        int five = 5;
-        int sixDivTwo = six / two;
-        int fiveMinusTwo = five - two;
-        int fourTimeTwo = four * two;
-        int onePlusTwo = one + two;
-        System.out.println(onePlusTwo);
-        System.out.println(sixDivTwo);
-        System.out.println(fiveMinusTwo);
-        System.out.println(fourTimeTwo);
-
-        int size = 10;
-        size = 100;
-        size = size - 5;
-        System.out.println(size);
-
+        Calculator.plus(1, 2);
+        Calculator.plus(10, 11);
+        Calculator.plus(100, 500);
+        Calculator.plus(4 , 2);
+        Calculator.plus(3, 5);
     }
 }
