5bd3188ea97463aece0c622ec306042b2ed2bf5d
jbioshock1912 <loh7457490>
2020-05-08 22:30:20 +0300

    Вам необходимо разработать программу "Глупый бот".
    Бот должен принимать строку и возвращать ответ.
    
    Бот наш глупый. Он умеет отвечать только на три Фразы.
    
    Если пришла фраза "Привет, Бот.", бот отвечает: "Привет, умник."
    
    Если пришла фраза "Пока.", бот отвечает: "До скорой встречи."
    
    Если пришла любая другая фраза,  бот отвечает: "Это ставит меня в тупик. Задайте другой вопрос."
    
    Помните, что строки можно сравнивать только через методы equals. Нельзя использовать ==.
    
    Давайте составим каркас класса.
    
    package ru.job4j.condition;
    
    public class DummyBot {
    
        public static String answer(String question) {
            String rsl = "Это ставит меня в тупик. Задайте другой вопрос.";
            if ("Привет, Бот.".equals(question)) {
                // заменить ... на правильный ответ rsl = "ответ по заданию".
                rsl = ...;
            } else if (....) { // заменить ... на проверку, известен ли боту этот вопрос и он знает как на него ответить.
                // заменить ... на правильный ответ rsl = "ответ по заданию".
                rsl = ..;
            }
            return rsl;
         }
    }

---
+++ src/main/java/ru/job4j/condition/DummyBot.java
@@ -0,0 +1,17 @@
+package ru.job4j.condition;
+
+public class DummyBot {
+    public static void main(String[] args) {
+        System.out.println(DummyBot.answer("Привет, Бот."));
+    }
+
+    public static String answer(String question) {
+        String rsl = "Это ставит меня в тупик. Задайте другой вопрос.";
+        if ("Привет, Бот.".equals(question)) {
+            rsl = "Привет, умник.";
+        } else if ("Пока.".equals(question)) {
+            rsl = "До скорой встречи.";
+        }
+        return rsl;
+    }
+}
