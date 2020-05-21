3034005d2fcab04f49ab84919411c4071f39af1f
jbioshock1912 <loh7457490>
2020-05-21 22:49:44 +0300

    . В предыдущем уроке вы создали класс ru.job4j.array.ArrayDefinition c методом main.
    2. Создайте внутри метода main массив String на 4 ячейки. Дайте имя переменной созданного массива - names.
    
    3. Заполните массив произвольными именами. Заполнение производить через обращение по индексу.

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -10,5 +10,9 @@ public class ArrayDefinition {
         names[1] = "Svyatko";
         names[2] = "Camilla";
         names[3] = "Sabirov";
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
     }
 }
