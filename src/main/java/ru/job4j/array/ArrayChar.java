e683de59b273df18ced9e9714fcb3dc86f97e2ad
jbioshock1912 <loh7457490>
2020-05-26 23:17:41 +0300

    В JDK есть класс String. В нем есть метод startsWith. Этот метод проверяет то, что слово начинается c определенной последовательности.
    Например.
    
    Привет - При - true
    
    Привет - Пре - false.
    
    В этом задании вам нужно будет написать свой аналогичный метод.
    
    Создайте каркас класса ru.job4j.array.ArrayChar.
    
    package ru.job4j.array;
    
    public class ArrayChar {
    
        public static boolean startsWith(char[] word, char[] pref) {
            boolean result = true;
            return result;
        }
    }
    Объяснение.
    
    Метод принимает два массива символов. В цикле for вам нужно сравнить значения ячеек с одинаковым индексом. Если все ячейки из массива pref соответствуют таким же ячейкам в массиве word, значит, нужно вернуть true.
    
    Если хотя бы один элемент отличается, значит, слово word не начинается со слова pref.
    
    Каркас теста.
    
    package ru.job4j.array;
    
    import org.junit.Test;
    import static org.hamcrest.core.Is.is;
    import static org.junit.Assert.assertThat;
    
    public class ArrayCharTest {
        @Test
        public void whenStartWithPrefixThenTrue() {
            char[] word = {'H', 'e', 'l', 'l', 'o'};
            char[] pref = {'H', 'e'};
            boolean result = ArrayChar.startsWith(word, pref);
            assertThat(result, is(true));
        }
    
        @Test
        public void whenNotStartWithPrefixThenFalse() {
            char[] word = {'H', 'e', 'l', 'l', 'o'};
            char[] pref = {'H', 'i'};
            boolean result =ArrayChar.startsWith(word, pref);
            assertThat(result, is(false));
        }
    }
     Задание.
    
    1. Доработайте требуемый функционал класса ru.job4j.array.ArrayChar.
    2. Залейте код в репозиторий. Оставьте ссылку на коммит.
    3. Выставите ответственного Петра Арсентьева.

--- src/main/java/ru/job4j/array/ArrayChar.java
+++ src/main/java/ru/job4j/array/ArrayChar.java
@@ -3,12 +3,12 @@ package ru.job4j.array;
 public class ArrayChar {
     public static boolean startsWith(char[] word, char[] pref) {
         boolean result = true;
-        for (int index = 0; index < word.length;) {
-            if ((word[index] == pref [index])) {
-
+        for (int index = 0; index < pref.length; index++) {
+            if (!(word[index] == pref[index])) {
+result = false;
+break;
             }
         }
-
         return result;
     }
 }
