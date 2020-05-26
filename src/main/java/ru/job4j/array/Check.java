446c037b08c07a9ea1f4b178741a61d1c49b94e0
jbioshock1912 <loh7457490>
2020-05-26 23:10:26 +0300

    1. Необходимо создать класс Check. 2. В классе Check написать метод public static boolean mono(boolean[] data).
    package ru.job4j.array;
    
    public class Check {
        public static boolean mono(boolean[] data) {
            boolean result = true;
    /*        for () { */
    /*        }        */
            return result;
        }
    }
    
    3. Метод должен проверить, что все элементы в массиве являются true или false.
    
    Решение задачи сводится к проверке всех элементов с эталонным. Эталонным можно взять любой элемент в массиве. Лучше взять элемент с индексом 0.
    
    Например:
    {true, true, true} - вернет true; массив однородный, он заполнен true.
    {true, false, true} - вернет false; массив не однородный, он заполнен и true и false.
    {false, false, false} - вернет true; массив однородный, он заполнен false.
    
    4. В задании нельзя использовать дополнительный массив.
    
    5. В классе CheckTest напишите 2 тест-метода, которые будут проверять вашу программу для случая, кода все элементы массива одинаковые и для случая, когда в массиве есть различающиеся элементы.
    6. В тестах можно сразу проверять целиком массив через метод assertThat(resultArray, is(expectArray)).
    7. Класс Check должен находиться в ...\src\ru\job4j\array\Check.java.
    8. Тест-класс CheckTest должен находиться в ...\test\ru\job4j\array\CheckTest.java.

--- src/main/java/ru/job4j/array/Check.java
+++ src/main/java/ru/job4j/array/Check.java
@@ -4,11 +4,9 @@ public class Check {
     public static boolean mono(boolean[] data) {
         boolean result = true;
         for (int index = 1; index < data.length; index++) {
-            if (data[index - 1] == data[index]) {
-                return result;
-            } else {
+            if (!(data[index - 1] == data[index])) {
                 result = false;
-                return result;
+                break;
             }
         }
         return result;
