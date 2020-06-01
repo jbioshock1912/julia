bcb5909223b91f452bd8a9138b21f0bffaef8cd0
jbioshock1912 <loh7457490>
2020-06-01 23:15:18 +0300

    Задание.
    1. Создайте класс ru.job4j.array.BarleyBreak с методом main.
    
    2. Внутри метода main объявите двойной массив 3 на 3.
    
    3. Заполните его цифрами от 1 до 9.
    
    4. Залейте код в репозиторий, оставьте ссылку на коммит, переведите ответственного на Петра Арсентьева.

--- src/main/java/ru/job4j/array/BarleyBreak.java
+++ src/main/java/ru/job4j/array/BarleyBreak.java
@@ -3,7 +3,7 @@ package ru.job4j.array;
 public class BarleyBreak {
 
     public static void main(String[] args) {
-        int[][] table = new int[2][2];
+        int[][] table = new int[3][3];
         table[0][0] = 9;
         table[0][1] = 9;
         table[0][2] = 9;
