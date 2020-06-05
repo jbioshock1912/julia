aac78189da58192681ede1e97df70af7f78a0ee3
jbioshock1912 <loh7457490>
2020-06-05 23:11:36 +0300

    6.8. Дефрагментация массива. [#225] Администратор, 05.03.20 2:44 В этом уроке необходимо написать код, который будет перемещать заполненные ячейки массива в начало, а пустые в конец. Когда мы создаем массив строк, то по умолчанию все ячейки в нем заполняются по null-ссылке. Про null-ссылку будет рассказано во второй части. Пока воспринимайте null-ссылку как переменную с пустым значением. Мы помним, что массивы в Java создаются с фиксированной длиной. То есть добавить новую ячейку или удалить старую ячейку нельзя. Можно только заполнить ячейку или освободить ее. При работе с массивами будут возникать ситуации, когда в массиве часть ячеек будет заполнена, а часть - нет. Заполненные ячейки могут быть разбросаны по всему массиву. Нам бы хотелось расположить все эти ячейки в начале массива.

---
+++ src/main/java/ru/job4j/array/Defragment.java
@@ -0,0 +1,29 @@
+package ru.job4j.array;
+
+public class Defragment {
+    public static String[] compress(String[] array) {
+        for (int index = 0; index < array.length; index++) {
+            if (array[index] == null) {
+                int point = index;
+                int indexWithChar = point;
+                while ((indexWithChar < array.length - 1) && array[indexWithChar] == null) {
+                    indexWithChar++;
+                }
+                String temp = array[indexWithChar];
+                array[indexWithChar] = array[point];
+                array[point] = temp;
+            }
+            System.out.print(array[index] + " ");
+        }
+        return array;
+    }
+
+    public static void main(String[] args) {
+        String[] input = {"I", null, "wanna", null, "be", null, "compressed"};
+        String[] compressed = compress(input);
+        System.out.println();
+        for (int index = 0; index < compressed.length; index++) {
+            System.out.print(compressed[index] + " ");
+        }
+    }
+}
