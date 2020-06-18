7dfd4779212e37d690226b163bd37a364133e66f
jbioshock1912 <loh7457490>
2020-06-18 23:59:27 +0300

    1. Создайте дополнительное поле private String name. Это поле должно содержать кличку котика. 2. Создайте метод public void giveNick(String nick). Этот метод должен записывать переменную nick в поле name. 3. Измените метод void show() так, чтобы при его вызове в консоль выводилась информация: имя котика и что он ел.

--- src/main/java/ru/job4j/oop/Cat.java
+++ src/main/java/ru/job4j/oop/Cat.java
@@ -1,13 +1,38 @@
 package ru.job4j.oop;
 
 public class Cat {
+
+    private String food;
+    private String name;
+
     public static void main(String[] args) {
-        Cat peppy = new Cat();
-        Cat sparky = new Cat();
+        System.out.println("There are gav's food.");
+        Cat gav = new Cat();
+        gav.eat("kotleta");
+        gav.giveNick("barsik");
+        gav.show();
+        System.out.println("There are black's food.");
+        Cat black = new Cat();
+        black.eat("fish");
+        black.giveNick("vaska");
+        black.show();
+    }
+
+    public void eat(String meat) {
+        this.food = meat;
+    }
+
+    public void show() {
+        System.out.println(this.food + ", " + this.name);
+
     }
 
     public String sound() {
         String voice = "may-may";
         return voice;
+    }
+
+    public void giveNick(String nick) {
+        this.name = nick;
     }
 }
