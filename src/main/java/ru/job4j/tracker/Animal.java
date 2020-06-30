19ac156f5899df9fb4f8fd738f2566206bd243ca
jbioshock1912 <loh7457490>
2020-06-30 22:43:32 +0300

    1. Вернитесь к заданию про конструкторы по умолчанию и добавьте в базовый класс Animal поле String name, которое должно инициализироваться через конструктор. Нужно перегрузить конструктор, а не заменить его ВОТ ЭТО НЕ ЯСНО
    2. Поправьте классы наследники, чтобы весь код после изменений был корректным.

--- src/main/java/ru/job4j/tracker/Animal.java
+++ src/main/java/ru/job4j/tracker/Animal.java
@@ -1,8 +1,15 @@
 package ru.job4j.tracker;
 
 public class Animal {
+
+    private String name;
+
     public Animal() {
         super();
         System.out.println("Animal");
+    }
+
+    public Animal(String name) {
+        this.name = name;
     }
 }
