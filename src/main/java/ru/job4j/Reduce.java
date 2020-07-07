057ab1f91e03ecc22b311fb0a519dbaa08819b53
jbioshock1912 <loh7457490>
2020-07-07 23:07:21 +0300

    2. Создайте этот класс в своем проекте. 3. В этом классе есть 5 нарушений оговоренный выше правил. - Для всех полей используем только модификатор private. - Для классов - public. Во второй и третьей части курса так же применим для классов private. - Для методов - public или private. Общее правило, если метод имеет модификатор default или protected его нужно переделать на public 4. Вам необходимо устранить эти нарушения.

--- src/main/java/ru/job4j/Reduce.java
+++ src/main/java/ru/job4j/Reduce.java
@@ -6,16 +6,17 @@ public class Reduce {
     public void to(int[] array) {
         this.array = array;
     }
+
     public void print() {
         for (int index = 0; index < array.length; index++) {
             System.out.println(array[index]);
         }
     }
+
     public static void main(String[] args) {
         int[] array = {1, 2, 3};
         Reduce reduce = new Reduce();
         reduce.to(array);
         reduce.print();
     }
-
 }
