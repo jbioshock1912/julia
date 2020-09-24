9f2ce73f9f09c8ca6afcc107f2e7d0a23eaf86e1
jbioshock1912 <loh7457490>
2020-09-24 19:36:36 +0300

    магический шар

--- src/main/java/ru/job4j/io/MagicBall.java
+++ src/main/java/ru/job4j/io/MagicBall.java
@@ -4,6 +4,7 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class MagicBall {
+
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.print("Я великий Оракул. Что ты хочешь узнать? ");
