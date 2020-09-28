0bc94306b11359818345626348376b1ee7f94cf7
jbioshock1912 <loh7457490>
2020-09-28 21:37:56 +0300

    спички идиотские

--- src/main/java/ru/job4j/io/Matches.java
+++ src/main/java/ru/job4j/io/Matches.java
@@ -1,46 +1,42 @@
 package ru.job4j.io;
 
+import org.w3c.dom.ls.LSOutput;
+
 import java.util.Scanner;
 
 public class Matches {
 
+    public static  int allMatches = 11;
+
     public int enterNumber() {
+
         Scanner input = new Scanner(System.in);
         int gamer = Integer.valueOf(input.nextLine());
         return gamer;
     }
 
     public boolean checkNumber(int number) {
-        return number <= 3 && number >= 1;
+        return number <= 3 && number >= 1 && allMatches >= number;
     }
 
     public static void main(String[] args) {
         Matches matches = new Matches();
-        int allMatches = 11;
+
         System.out.println(allMatches);
         System.out.println("начальное количество спичек: 11");
         boolean winner = true;
 
         do {
-            if (winner) {
-                System.out.println("ходит 1 игрок");
-            } else {
-                System.out.println("ходит 2 игрок");
-            }
+            System.out.println(winner ? "ходит 1 игрок" : "ходит 2 игрок");
             int numberOfMatches = matches.enterNumber();
             if (!matches.checkNumber(numberOfMatches)) {
-                System.out.println("введите число от 1 до 3 включительно");
+                System.out.println("введите число от 1 до 3 включительно или число не больше, чем оставшееся количество спичек");
             } else {
                 allMatches = allMatches - numberOfMatches;
                 System.out.println(allMatches);
                 winner = !winner;
             }
         } while (allMatches >= 1);
-        if (!winner) {
-            System.out.println("победил игрок 1");
-
-        } else {
-            System.out.println("победил игрок 2");
-        }
+        System.out.println(winner ? "победил игрок 2" : "победил игрок 1");
     }
 }
