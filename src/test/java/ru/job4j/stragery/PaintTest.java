acbc6408ae211d7ffb69ea8eeaade3ca2270e7d0
jbioshock1912 <loh7457490>
2020-10-12 22:03:31 +0300

    тесты треугольник и квадрат

--- src/test/java/ru/job4j/stragery/PaintTest.java
+++ src/test/java/ru/job4j/stragery/PaintTest.java
@@ -1,24 +1,41 @@
-package ru.job4j.stragery;
-
+package ru.job4j.pseudo;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+import ru.job4j.stragery.Paint;
+import ru.job4j.stragery.Square;
+import ru.job4j.stragery.Triangle;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 import java.util.StringJoiner;
-
-
-import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.*;
-
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+/**
+ * @author Petr Arsentev (parsentev@yandex.ru)
+ * @version $Id$
+ * @since 0.1
+ */
 public class PaintTest {
+    // поле содержит дефолтный вывод в консоль.
+    private final PrintStream stdout = System.out;
+    // буфер для результата.
+    private final ByteArrayOutputStream out = new ByteArrayOutputStream();
+    @Before
+    public void loadOutput() {
+        System.out.println("execute before method");
+        System.setOut(new PrintStream(this.out));
+    }
+    @After
+    public void backOutput() {
+        System.setOut(this.stdout);
+        System.out.println("execute after method");
+    }
     @Test
     public void whenDrawSquare() {
-        PrintStream stdout = System.out;
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(out));
         new Paint().drawFigure(new Square());
         assertThat(
-                out.toString(),
+                this.out.toString(),
                 is(
                         new StringJoiner(System.lineSeparator())
                                 .add("+++++")
@@ -29,17 +46,12 @@ public class PaintTest {
                                 .toString()
                 )
         );
-        System.setOut(stdout);
     }
-
     @Test
     public void whenDrawTriangle() {
-        PrintStream stdout = System.out;
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(out));
         new Paint().drawFigure(new Triangle());
         assertThat(
-                out.toString(),
+                this.out.toString(),
                 is(
                         new StringJoiner(System.lineSeparator())
                                 .add("   +    ")
@@ -50,6 +62,5 @@ public class PaintTest {
                                 .toString()
                 )
         );
-        System.setOut(stdout);
     }
 }
